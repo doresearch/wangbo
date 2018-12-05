75ff5541482f4bfa3859afa283e2bcfe945b9d74
wangbo <wangbo@fastschool.cn>
2018-12-05 16:24:29 +0800

    自动提交

--- Wmobile/src/index/index.js
+++ Wmobile/src/index/index.js
@@ -1,2 +1,24 @@
 import '../../lib/css/normalize.css'
 import '../../lib/css/w.css'
+
+function getPos() {
+  let fingePos = {
+    x: 0,
+    y: 0
+  }
+
+  document.documentElement.addEventListener('mousemove', res => {
+    fingePos.x = res.x
+    fingePos.y = res.y
+  })
+
+  return fingePos
+}
+
+let fingePos = getPos()
+
+// let address = document.getElementsByTagName('address')[0]
+
+document.addEventListener('touchstart', ev => {
+  ev.preventDefault()
+})
